0898f17db0a0bb1becbffd7c38a773c56fbecba8
lipskidi <lipski.dima11@gmail.com>
2025-04-21 23:48:27 +0300

    dz5.2

--- lesson6/lesson6.1.py
+++ lesson6/lesson6.1.py
@@ -1,6 +1,9 @@
-x = b"test"
+import string
 
-
-
-print(type(x))
-print(x)
+x = input("Enter 2 letters across -: ")
+start_ch, end_ch = x.split('-')
+y = string.ascii_letters
+start_i = y.index(start_ch)
+end_i = y.index(end_ch)
+res = y[start_i:end_i + 1]
+print(res)
