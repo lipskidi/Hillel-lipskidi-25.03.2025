0a3a14fefbf39ad0b39b005872ce4cd4bb861aa0
lipskidi <lipski.dima11@gmail.com>
2025-04-24 00:13:57 +0300

    dz7.1

--- lesson7/lesson7.1.py
+++ lesson7/lesson7.1.py
@@ -1,4 +1,6 @@
 def say_hi(name, age):
     return f"Hi. My name is {name} and I'm {age} years old"
-res = say_hi("Dumbledore", 115)
-print(res)
+assert say_hi("Alex", 21)
+assert say_hi("Frank", 68)
+print(say_hi("Alex", 32))
+print(say_hi("Frank", 68))
