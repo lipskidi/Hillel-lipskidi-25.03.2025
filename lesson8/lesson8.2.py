96473d45ff9f001312ae946a9fa8ca88315bda7c
lipskidi <lipski.dima11@gmail.com>
2025-05-05 18:59:10 +0300

    dz7.1

---
+++ lesson8/lesson8.2.py
@@ -0,0 +1,12 @@
+def is_palindrome(text: str) -> bool:
+    text = text.lower()
+    cleaned = ''
+    for ch in text:
+        if ch.isalnum():
+            cleaned += ch
+    return cleaned == cleaned[::-1]
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+print("OK")
